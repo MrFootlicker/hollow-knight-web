94ae36f2725f3ca1962c97ec581ae6d3adeb4f5a
MrFootlicker <ijatiscool@gmail.com>
2026-02-08 12:27:17 +0300

    Add files via upload

--- enemy.js
+++ enemy.js
@@ -1,18 +1,18 @@
 const enemies = [
-  {x:600,y:344,w:26,h:26,hp:3,dir:1}
+  {x:500,y:432,w:32,h:32,hp:3,dir:1},
+  {x:800,y:272,w:32,h:32,hp:3,dir:-1}
 ];
 
 function updateEnemies(){
   enemies.forEach(e=>{
     if(e.hp<=0) return;
-    e.x += e.dir*1.2;
+    e.x += e.dir * 1.2;
     if(Math.abs(e.x-player.x)>200) e.dir*=-1;
   });
 }
 
 function drawEnemies(ctx){
-  ctx.fillStyle="#ff6b6b";
   enemies.forEach(e=>{
-    if(e.hp>0) ctx.fillRect(e.x,e.y,e.w,e.h);
+    if(e.hp>0) ctx.drawImage(assets.enemy, e.x, e.y, e.w, e.h);
   });
 }
